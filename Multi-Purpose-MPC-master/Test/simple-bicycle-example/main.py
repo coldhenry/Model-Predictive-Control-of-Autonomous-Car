edc91092422461ada2cdf23fc635e2ff36b852cd
coldhenry <yul133@eng.ucsd.edu>
2021-03-10 01:35:29 -0800

    basic structural integration finish

--- Multi-Purpose-MPC-master/Test/simple-bicycle-example/main.py
+++ Multi-Purpose-MPC-master/Test/simple-bicycle-example/main.py
@@ -1,14 +1,15 @@
-from simulator import simulator
-from MPC import MPC
+from reference_path import ReferencePath
+from map import Map, Obstacle
 from model import simple_bycicle_model
+from MPC import MPC
+import pdb
+import sys
+import time
 import do_mpc
 import numpy as np
 import matplotlib.pyplot as plt
 from casadi import *
 from casadi.tools import *
-import pdb
-import sys
-import time
 sys.path.append('../../')
 
 
@@ -16,54 +17,113 @@ sys.path.append('../../')
 show_animation = True
 store_results = False
 
+# Load map file
+map = Map(file_path='maps/sim_map.png', origin=[-1, -2],
+          resolution=0.005)
+
+# Specify waypoints
+wp_x = [-0.75, -0.25, -0.25, 0.25, 0.25, 1.25, 1.25, 0.75, 0.75, 1.25,
+        1.25, -0.75, -0.75, -0.25]
+wp_y = [-1.5, -1.5, -0.5, -0.5, -1.5, -1.5, -1, -1, -0.5, -0.5, 0, 0,
+        -1.5, -1.5]
+
+# Specify path resolution
+path_resolution = 0.05  # m / wp
+
+# Create smoothed reference path
+reference_path = ReferencePath(map, wp_x, wp_y, path_resolution,
+                               smoothing_distance=5, max_width=0.23,
+                               circular=True)
+
+# Add obstacles
+use_obstacles = True
+if use_obstacles:
+    obs1 = Obstacle(cx=0.0, cy=0.0, radius=0.05)
+    obs2 = Obstacle(cx=-0.8, cy=-0.5, radius=0.08)
+    obs3 = Obstacle(cx=-0.7, cy=-1.5, radius=0.05)
+    obs4 = Obstacle(cx=-0.3, cy=-1.0, radius=0.08)
+    obs5 = Obstacle(cx=0.27, cy=-1.0, radius=0.05)
+    obs6 = Obstacle(cx=0.78, cy=-1.47, radius=0.05)
+    obs7 = Obstacle(cx=0.73, cy=-0.9, radius=0.07)
+    obs8 = Obstacle(cx=1.2, cy=0.0, radius=0.08)
+    obs9 = Obstacle(cx=0.67, cy=-0.05, radius=0.06)
+    map.add_obstacles([obs1, obs2, obs3, obs4, obs5, obs6, obs7,
+                       obs8, obs9])
+
+
 """
 Get configured do-mpc modules:
 """
-model = simple_bycicle_model()
+# model setup
+model = simple_bycicle_model(length=0.12, width=0.06,
+                             reference_path=reference_path, Ts=0.05).model_setup()
 mpc = MPC(model).mpc
-simulator = simulator(model)
 estimator = do_mpc.estimator.StateFeedback(model)
 
+# Compute speed profile
+ay_max = 4.0  # m/s^2
+a_min = -0.1  # m/s^2
+a_max = 0.5  # m/s^2
+SpeedProfileConstraints = {'a_min': a_min, 'a_max': a_max,
+                           'v_min': 0.0, 'v_max': 1.0, 'ay_max': ay_max}
+model.reference_path.compute_speed_profile(SpeedProfileConstraints)
 
 """
 Set initial state
 """
-np.random.seed(99)
-
-e = np.ones([model.n_x, 1])
-x0 = np.random.uniform(-3*e, 3*e)  # Values between +3 and +3 for all states
+x0 = np.array([wp_x[0], wp_y[1], 0, 0, 0, 0])
 mpc.x0 = x0
-simulator.x0 = x0
-estimator.x0 = x0
 
 # Use initial state to set the initial guess.
 mpc.set_initial_guess()
 
-"""
-Setup graphic:
-"""
-
-fig, ax, graphics = do_mpc.graphics.default_plot(mpc.data)
-plt.ion()
 
 """
 Run MPC main loop:
 """
 
-for k in range(50):
-    u0 = mpc.make_step(x0)
-    y_next = simulator.make_step(u0)
-    x0 = estimator.make_step(y_next)
+##############
+# Simulation #
+##############
 
-    if show_animation:
-        graphics.plot_results(t_ind=k)
-        graphics.plot_predictions(t_ind=k)
-        graphics.reset_axes()
-        plt.show()
-        plt.pause(0.01)
+# Set simulation time to zero
+t = 0.0
+
+# Logging containers
+x_log = [model.x['pos_x']]
+y_log = [model.x['pos_y']]
+v_log = [0.0]
+
+# Until arrival at end of path
+while model.s < reference_path.length:
+
+    # Get control signals
+    u = mpc.get_control()
+
+    # Simulate car
+    model.drive(u)
+
+    # Log car state
+    x_log.append(model.x['pos_x'])
+    y_log.append(model.x['pos_y'])
+    v_log.append(model.x['vel'])
+
+    # Increment simulation time
+    t += model.Ts
+
+    # Plot path and drivable area
+    reference_path.show()
+
+    # Plot car
+    model.show()
+
+    # Plot MPC prediction
+    mpc.show_prediction()
+
+    # Set figure title
+    plt.title('MPC Simulation: acc(t): {:.2f}, delta(t): {:.2f}, Duration: '
+              '{:.2f} s'.format(u[0], u[1], t))
+    plt.axis('off')
+    plt.pause(0.001)
 
 input('Press any key to exit.')
-
-# Store results:
-if store_results:
-    do_mpc.data.save_results([mpc, simulator], 'oscillating_masses')
