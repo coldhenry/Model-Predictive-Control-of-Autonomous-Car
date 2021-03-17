2dad0efdd6867345f56a123e3fa9ba05156f7945
Arthur Hsieh <arthur960304@gmail.com>
2021-03-17 00:42:33 -0700

    finish path tracking

--- Multi-Purpose-MPC-master/Test/simple-bicycle-example/MPC.py
+++ Multi-Purpose-MPC-master/Test/simple-bicycle-example/MPC.py
@@ -67,16 +67,21 @@ class MPC:
         return self.tvp_template
 
     def objective_function_setup(self):
-        lterm = ((self.model.aux['e_y'] ** 2 + self.model.aux['e_psi'] ** 2) + ((self.model.x['pos_x'] - self.model.tvp['x_ref']) ** 2
-                + (self.model.x['pos_y'] - self.model.tvp['y_ref']) ** 2
-                + 5 * (self.model.aux['psi_cost']) ** 2
-                + (self.model.x['vel'] - self.model.tvp['vel_ref']) ** 2))
-        mterm = (((self.model.x['pos_x'] - self.model.tvp['x_ref']) ** 2
-                + (self.model.x['pos_y'] - self.model.tvp['y_ref']) ** 2
-                + (self.model.x['vel'] - self.model.tvp['vel_ref']) ** 2))
+        lterm = (
+            self.model.aux['e_y'] ** 2 
+            + self.model.aux['e_psi'] ** 2 
+            + self.model.aux['psi_cost'] ** 2 
+            + 10 * (self.model.x['pos_x'] - self.model.tvp['x_ref']) ** 2 
+            + 10 * (self.model.x['pos_y'] - self.model.tvp['y_ref']) ** 2)
+            # + (self.model.x['vel'] - self.model.tvp['vel_ref']) ** 2)
+
+        mterm = (
+            self.model.x['pos_x'] - self.model.tvp['x_ref'] ** 2 
+            + (self.model.x['pos_y'] - self.model.tvp['y_ref']) ** 2 
+            + (self.model.x['vel'] - self.model.tvp['vel_ref']) ** 2)
 
         self.mpc.set_objective(mterm=mterm, lterm=lterm)
-        self.mpc.set_rterm(acc=0.1, delta=0.01)
+        self.mpc.set_rterm(delta=0.01)
 
     def constraints_setup(
         self, vel_bound=[0.0, 1.0], e_y_bound=[0.0, 1.0], reset=False
